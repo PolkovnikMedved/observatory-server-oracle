20413f6dff62f0cb33cc758d5ba877430040f829
= <viktor.solodoukhin@groups.be>
2019-01-04 11:13:39 +0100

    Change structure element type on edit

--- src/main/java/be/solodoukhin/controller/StructureElementController.java
+++ src/main/java/be/solodoukhin/controller/StructureElementController.java
@@ -1,7 +1,9 @@
 package be.solodoukhin.controller;
 
 import be.solodoukhin.domain.StructureElement;
+import be.solodoukhin.domain.api.ErrorResponse;
 import be.solodoukhin.repository.StructureElementRepository;
+import be.solodoukhin.repository.StructureRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,22 +27,33 @@ import java.util.Optional;
 public class StructureElementController {
     private static final Logger LOGGER = LoggerFactory.getLogger(StructureElementController.class);
     private StructureElementRepository repository;
+    private StructureRepository structureRepository;
 
     @Autowired
-    public StructureElementController(StructureElementRepository repository) {
+    public StructureElementController(StructureElementRepository repository, StructureRepository structureRepository) {
         this.repository = repository;
+        this.structureRepository = structureRepository;
     }
 
     @PutMapping("/update")
-    public ResponseEntity<StructureElement> update(@RequestBody StructureElement el) {
+    public ResponseEntity<?> update(@RequestBody StructureElement el) {
         LOGGER.info("Call to StructureElementController.update with element number = " + el.getId());
         Optional<StructureElement> element = repository.findById(el.getId());
         if(element.isPresent()) {
+
+            if(el.getTypeStructure() != null) {
+                if (this.structureRepository.existsById(el.getTypeStructure().getName()) && (!el.getTypeStructure().getName().equals(element.get().getTypeStructure().getName()))) {
+                    element.get().setTypeStructure(this.structureRepository.getOne(el.getTypeStructure().getName()));
+                } else {
+                    return ResponseEntity.badRequest().body(new ErrorResponse(400, "Could not find new element type"));
+                }
+            }
 
             element.get().setTag(el.getTag().orElse(null));
             element.get().setDescription(el.getDescription());
             element.get().setOptional(el.isOptional());
             element.get().setRepetitive(el.isRepetitive());
+
             return ResponseEntity.ok(repository.save(element.get()));
         }
         else
