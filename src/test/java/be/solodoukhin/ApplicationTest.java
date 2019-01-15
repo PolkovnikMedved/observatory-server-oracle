2f9afa01419fd9471a0fac2321efa49904cd138e
= <viktor.solodoukhin@groups.be>
2019-01-15 17:10:19 +0100

    VersionController endpoints test with MockMvc

--- src/test/java/be/solodoukhin/ApplicationTest.java
+++ src/test/java/be/solodoukhin/ApplicationTest.java
@@ -1,6 +1,10 @@
 package be.solodoukhin;
 
 import be.solodoukhin.tool.TnsAdmin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
@@ -27,9 +31,21 @@ public abstract class ApplicationTest {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(ApplicationTest.class);
 
+    protected static ObjectMapper mapper;
+
     @BeforeClass
     public static void test_setUpTNS() {
         TnsAdmin.init();
         LOGGER.info("TNS : " + System.getProperty("oracle.net.tns_admin"));
+    }
+
+    @BeforeClass
+    public static void test_setUpObjectMapper() {
+        mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.registerModule(new Jdk8Module()); // Workaround for Optional fields
+        mapper.registerModule(new JavaTimeModule()); // Workaround for LocalDate fields
     }
 }
